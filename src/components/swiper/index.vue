8473cef4fa31d5291634e5ee7e9d99dc6c03c4a5
yangmin18 <yangmin18@58.com>
2021-04-15 13:47:32 +0800

    swiper

--- src/components/swiper/index.vue
+++ src/components/swiper/index.vue
@@ -7,9 +7,54 @@
 <script>
 export default {
   name: 'swiper',
+  props: {
+    autoplay: {
+      type: Boolean,
+      default: true
+    },
+    delay: {
+      type: Number,
+      default: 1000
+    },
+    initialIndex: {
+      type: Number,
+      default: 0
+    },
+    loop: {
+      type: Boolean,
+      default: true
+    }
+  },
   data(){
     return {
-
+      currentSelected: this.initialIndex,
+      len: 0, // item 的个数
+    }
+  },
+  mounted(){
+    this.len = this.$children.filter(child => {
+      return child.$options.name == 'swiper-item'
+    }).length;
+    this.run();
+  },
+  methods: {
+    run(){
+      if (this.autoplay) {
+        this.timer = setInterval(() => {
+          let index = this.currentSelected;
+          let newIndex = index + 1;
+          if (newIndex === this.len){
+            newIndex = 0;
+          }
+          if (newIndex === -1){
+            newIndex = this.len - 1;
+          }
+          this.currentSelected = newIndex
+        }, this.delay)
+      }
+    },
+    beforeDestroy(){
+      clearInterval(this.timer)
     }
   }
 }
--- src/components/swiper/swiper-item.vue
+++ src/components/swiper/swiper-item.vue
@@ -1,16 +1,44 @@
 <template>
-  <div class="swiper">
-    <slot></slot>
-  </div>
+  <transition>
+    <div class="swiper" v-if="isShow">
+      <slot></slot>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: 'swiper-item',
   data(){
+    let children = this.$parent.$children.filter(child => {
+      return child.$options.name === 'swiper-iterm'
+    })
     return {
-
+      index: children.length - 1
+    }
+  },
+  computed: {
+    isShow(){
+      return this.$parent.currentSelected === this.index
     }
   }
 }
 </script>
+
+<style lang="less" scoped>
+.v-enter-active, v-leave-active {
+  transition: all .5s linear;
+}
+.v-leave-to{
+  transform: translateX(-100%);
+}
+.v-enter{
+  transform: translateX(100%);
+}
+.v-enter-active{
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+}
+</style>
